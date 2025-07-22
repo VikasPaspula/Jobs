1cb4f00288a71517b4c5f1cf8be2412d318a24eb
VikasPaspula <vikkeevikas7@gmail.com>
2025-07-22 20:22:59 +0530

    Added root view for backend

--- backend/jobportal/urls.py
+++ backend/jobportal/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import JobListView
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "Welcome to the Job Portal Backend API"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_view), 
     path('api/', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
