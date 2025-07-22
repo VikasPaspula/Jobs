ad280c085edf69f2a1c045bd7a8968d625ef34b3
VikasPaspula <vikkeevikas7@gmail.com>
2025-07-22 19:41:37 +0530

    Make build.sh executable and updated paths

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# Install Python dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files (if using static files)
+python manage.py collectstatic --noinput
